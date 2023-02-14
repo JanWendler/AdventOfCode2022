b95a332ca3de7299cadf1d6c848ce528fab9cee6
Jan Wendler <jan.wendler@ost.ch>
2023-02-14 13:41:26 +0100

    day 1 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,28 +2,34 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("./src/input.txt").expect("").replace("\r\n", "\n");;
+    let content = fs::read_to_string("./src/input.txt").expect("").replace("\r\n", "\n");
     println!("{}", most_calories(content.as_str()));
 }
 
 fn most_calories(s: &str) -> u32 {
-    let mut biggest = 0;
+    let mut biggest:[u32;3] = [0,0,0];
     let mut temp = 0;
     for elf in s.split("\n\n"){
         temp = elf.lines().map(|line| line.parse::<u32>().expect("")).sum();
-        if temp > biggest{
-            biggest = temp;
+        if temp > biggest[0]{
+            biggest[2] = biggest[1];
+            biggest[1] = biggest[0];
+            biggest[0] = temp;
+        } else if temp > biggest[1]{
+            biggest[2] = biggest[1];
+            biggest[1] = temp;
+        } else if  temp > biggest[2]{
+            biggest[2] = temp;
         }
+
     }
-    biggest
+    biggest.iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn biggestElf() {
-        let str = "1000
+    const CONTENT: &str = "1000
 2000
 3000
 
@@ -37,8 +43,13 @@ mod tests {
 9000
 
 10000";
-
-        assert_eq!(most_calories(str), 24000);
+    #[test]
+    fn biggest_elf() {
+        assert_eq!(most_calories(CONTENT), 24000);
+    }
+    #[test]
+    fn part2() {
+        assert_eq!(most_calories(CONTENT), 45000);
     }
 
 
