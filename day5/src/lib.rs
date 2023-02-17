c2245708f90c1ce7d51ff914cc9adfd51936f91f
Jan Wendler <jan.wendler@ost.ch>
2023-02-17 11:51:03 +0100

    finished day 5 part 1

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::{Result};
 
-pub fn part1(s:&str) -> Result<i32> {
+pub fn part1(s:&str) -> Result<String> {
+    let mut res: String = String::new();
     let mut stacks: Vec<String> = vec!["".to_string(),"".to_string()];
     let (t1, t2) = s.split_once("\n\n").expect("");
     for line in t1.lines(){
@@ -12,8 +13,43 @@ pub fn part1(s:&str) -> Result<i32> {
             }
         }
     }
-    stacks = stacks.iter().map(|stack| stack.chars().rev().collect::<String>()).collect();
-    Ok(2)
+    stacks = stacks.iter().map(|stack| stack.chars().rev().collect::<String>().trim().to_string()).collect();
+    let mut moves: Vec<Vec<u32>> = vec![];
+    for line in t2.lines(){
+        let tmp = line
+            .replace("move ", "")
+            .replace("from ", "")
+            .replace("to ","")
+            .splitn(3, " ")
+            .map(|s,| s.parse().expect(""))
+            .collect::<Vec<u32>>();
+        moves.push(tmp);
+    }
+    /*
+    let moves: Vec<Vec<u32>> = t2
+        .replace("move ", "")
+        .replace("from ", "")
+        .replace("to ","")
+        .lines()
+        .rev()
+        .map(|s|{
+            s.splitn(3, " ")
+                .map(|s| {
+                    s.parse().expect("")})
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
+*/
+    for line in moves.iter(){
+        for _ in 0..line[0]{
+            let s = stacks[(line[1]-1) as usize].pop().expect("");
+            stacks[(line[2]-1) as usize].push(s);
+        }
+    }
+    for stack in stacks.iter_mut(){
+        res.push(stack.pop().expect(""));
+    }
+    Ok(res)
 }
 
 pub fn part2(s:&str) -> Result<i32> {
@@ -34,7 +70,7 @@ move 2 from 2 to 1
 move 1 from 1 to 2";
     #[test]
     fn test_part1() {
-        assert_eq!(part1(CONTENT).expect(""), 10);
+        assert_eq!(part1(CONTENT).expect(""), "CMZ".to_string());
     }
     #[test]
     fn test_part2() {
--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,7 +3,7 @@ use anyhow::{Context, Result};
 
 fn main()->Result<()>{
     let content = fs::read_to_string("./src/input.txt")?.replace("\r\n", "\n");
-    println!("Result of part 1 = {}", day2::part1(content.as_str()).context("")?);
-    println!("Result of part 2 = {}", day2::part2(content.as_str()).context("")?);
+    println!("Result of part 1 = {}", day5::part1(content.as_str()).context("")?);
+    println!("Result of part 2 = {}", day5::part2(content.as_str()).context("")?);
     Ok(())
 }
